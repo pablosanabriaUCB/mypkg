e1b0a865e8c54284e2a11343ae1a40bdd8f40c5c
pablosanabriaUCB <pablo.sanabria@ucb.edu.bo>
2026-02-23 07:52:25 -0400

    New project

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+package_name = 'mypkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='pablo',
+    maintainer_email='pablo.sanabria@ucb.edu.bo',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'pubclases = mypkg.pub:main',
+            'subclases = mypkg.sub:main',
+        ],
+    },
+)
